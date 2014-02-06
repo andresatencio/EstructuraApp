0d8dc6dcd827c290017e091bfc702fc587bcf3db
Andres Atencio <and.atencio@gmail.com>
2014-02-06 16:25:36 -0300

    page

---
+++ js/app/app.js
@@ -0,0 +1,10 @@
+$(function() {
+	FastClick.attach(document.body);
+	//alert('Hola mundo')
+	$('#botonaso').click(function(){
+		alert('Con');
+	});
+	$('#botoncito').click(function(){
+		alert('Sin');
+	});
+})

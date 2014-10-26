e08c878bd281624215e498efa08a05684a2dd80f
Andres <and.atencio@gmail.com>
2014-10-26 19:42:10 -0300

    Update app.js

--- js/app/app.js
+++ js/app/app.js
@@ -1,6 +1,11 @@
 $(function() {
 	FastClick.attach(document.body);
 	//alert('Hola mundo')
+	$.ajax({cache:false});
+	$.mobile.transitionFallbacks.slideout = "none";
+	$.mobile.defaultDialogTransition = "none";
+	$.mobile.defaultPageTransition = "none";
+	$.support.cors = true;
 	$('#botonaso').click(function(){
 		alert('Con');
 	});
